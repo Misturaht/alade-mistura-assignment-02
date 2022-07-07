7dda18bf1d800ef6c4fb5cc1627befea3a46c7a4
Alade Misturah <46754000+Misturaht@users.noreply.github.com>
2022-07-07 14:17:53 +0000

    Challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -3,33 +3,38 @@
 const assignment = {};
 // ========================
 
-
 /**
  * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   sum = 0  
+  for (i = 0; i < arrayOfNumbers.length; i++){
+    sum += arrayOfNumbers[i]
+  }
+  console.log(sum)
+  return sum
 }
+sumOfNumbers([1,2,3,4,5,6,7,8,9,10])
+
 // assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
  * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
  */
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+let evenNumber = [];
+for(i = 0; i < arrayOfNumbers.length; i++) {
+  if(arrayOfNumbers[i] % 2 == 0) {
+    evenNumber.push(arrayOfNumbers[i]);
+    console.log(arrayOfNumbers[i])
+  }
 }
+  console.log("The even numbers here are: " + evenNumber + " and their total number is " + evenNumber.length)
+return evenNumber;
+}
+countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 // assignment.countEvenNumbers = countEvenNumbers;
 
 /**
